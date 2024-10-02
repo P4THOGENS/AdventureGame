f54028b3a84816c19bcd281b1c5fe06fbe30c971
Saad <saadaas999@gmail.com>
2024-10-02 15:00:49 -0500

    Add game script

--- game.sh
+++ game.sh
@@ -3,7 +3,7 @@ echo "Welcome to the Adventure Game!"
 echo "Choose a location to explore: forest, castle, or cave."
 read location
 if [ "$location" == "forest" ]; then
-	cat forest.txtif 
+	cat forest.txt
 elif [ "$location" == "castle" ]; then
 	cat castle.txt
 elif [ "$location" == "cave" ]; then
